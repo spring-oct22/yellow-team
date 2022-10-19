5255a623f8b27b21df228ae2fcf9a930bf8234dd
AlexK <alexkendallmg@hotmail.com>
2022-10-19 08:19:58 +0100

    Aut

--- src/main/java/com/example/winery/controller/WineController.java
+++ src/main/java/com/example/winery/controller/WineController.java
@@ -25,7 +25,6 @@ public class WineController {
     @GetMapping("/wine/{id}")
     public WineDTO getWine(@PathVariable int id) {
         return wineService.getWine(id);
-
     }
 
     @DeleteMapping("/wine/{id}")
